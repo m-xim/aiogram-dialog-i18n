8533029d38fa7d0b152c971c6de881a421f2efef
m-xim <170838360+m-xim@users.noreply.github.com>
2026-09-24 21:46:22 +0300

    refactor: import widgets only from the library modules

--- src/aiogram_dialog_i18n/__init__.py
+++
@@ -1,6 +0,0 @@
-from aiogram_dialog_i18n.aiogram_i18n import I18nFormat
-from aiogram_dialog_i18n.fluentogram import FluentogramFormat
-
-T = I18nFormat
-
-__all__ = ["FluentogramFormat", "I18nFormat", "T"]
